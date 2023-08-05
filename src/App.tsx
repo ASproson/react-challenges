4a7fc1dcc8d0788236a49834821db928cb1b900d
atlas <a.sproson@outlook.com>
2023-08-05 17:17:20 +0100

    Adds demo route

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import { Link } from "react-router-dom";
 
 function App() {
-  return <div className="App text-3xl font-bold text-white">Hello world</div>;
+  return (
+    <>
+      <div className="App text-3xl font-bold text-white">Hello world</div>
+      <Link to={"/demo"}>
+        <div className="text-white">TO DEMO</div>
+      </Link>
+    </>
+  );
 }
 
 export default App;

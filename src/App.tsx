070268ba62e9355ffa01321e43117c2991a09249
atlas <a.sproson@outlook.com>
2023-08-05 17:05:20 +0100

    Darkens bg

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <div className="App text-3xl font-bold">Hello world</div>;
+  return <div className="App text-3xl font-bold text-white">Hello world</div>;
 }
 
 export default App;

11572f1744267d4679258e3a7fb50b3286ea1f81
atlas <a.sproson@outlook.com>
2023-08-06 10:42:32 +0100

    Abstracts rendering of completed to dos to component

---
+++ src/components/todos/ShowCompletedToDos.tsx
@@ -0,0 +1,25 @@
+import { CompletedToDoList } from './CompletedToDoList';
+import { ToDoItem } from './Todos';
+
+interface ShowCompletedToDosProps {
+  completedToDos: ToDoItem[];
+}
+
+export const ShowCompletedToDos = ({
+  completedToDos,
+}: ShowCompletedToDosProps) => {
+  return (
+    <div>
+      <h1 className="text-white text-2xl">Completed ToDos</h1>
+
+      {completedToDos?.map((completedToDo: ToDoItem) => {
+        return (
+          <CompletedToDoList
+            key={completedToDo.id}
+            completedToDo={completedToDo}
+          />
+        );
+      })}
+    </div>
+  );
+};

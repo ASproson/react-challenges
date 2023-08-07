9234fcb5a7f7a616379eb6c2989762839472ba5e
atlas <a.sproson@outlook.com>
2023-08-07 08:07:08 +0100

    Removes Demo component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
-import "./App.css";
-import { Link } from "react-router-dom";
+import './App.css';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <div className="App text-3xl font-bold text-white">Hello world</div>
       <div>
-        <Link to={"/demo"} className="text-white">
-          To Demo
-        </Link>
-      </div>
-      <div>
-        <Link to={"/todos"} className="text-white">
+        <Link to={'/todos'} className="text-white">
           Todos
         </Link>
       </div>

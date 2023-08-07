9f94cc894842d96382b3b1c2712bd5c19f2b52b4
atlas <a.sproson@outlook.com>
2023-08-07 08:26:52 +0100

    Creates custom hook to manage adding todo

---
+++ src/components/todos/hooks/useAddToDo.tsx
@@ -0,0 +1,29 @@
+import { useCallback } from 'react';
+const { v4: uuidv4 } = require('uuid');
+
+interface ToDoItem {
+  id: string;
+  title: string;
+}
+
+interface useAddToDoProps {
+  newToDo: string;
+  setNewToDo: React.Dispatch<React.SetStateAction<string>>;
+  notifySuccessfulToDoAddition: (newTask: string) => void;
+  setToDos: React.Dispatch<React.SetStateAction<ToDoItem[]>>;
+}
+
+export const useAddToDo = ({
+  setToDos,
+  notifySuccessfulToDoAddition,
+  setNewToDo,
+  newToDo,
+}: useAddToDoProps) => {
+  return useCallback(() => {
+    if (!newToDo) return;
+    const newTask: ToDoItem = { id: uuidv4(), title: newToDo };
+    setToDos((prev) => [...prev, newTask]);
+    notifySuccessfulToDoAddition(newTask.title);
+    setNewToDo('');
+  }, [setToDos, notifySuccessfulToDoAddition, setNewToDo, newToDo]);
+};

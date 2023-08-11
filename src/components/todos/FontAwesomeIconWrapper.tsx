fa382217d7e919ac74b43ffb95ba082d0016fb85
atlas <a.sproson@outlook.com>
2023-08-11 18:27:36 +0100

    Organizes imports

--- src/components/todos/FontAwesomeIconWrapper.tsx
+++ src/components/todos/FontAwesomeIconWrapper.tsx
@@ -1,6 +1,6 @@
-import { IconDefinition } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import clsx from 'clsx';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { IconDefinition } from '@fortawesome/free-solid-svg-icons';
 
 interface FontAwesomeIconWrapperProps {
   icon: IconDefinition;

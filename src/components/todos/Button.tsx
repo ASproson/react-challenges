3575c92f38e0cd77482015527165cc5c47ca0ea5
atlas <a.sproson@outlook.com>
2023-08-11 15:31:48 +0100

    Add some base styling

--- src/components/todos/Button.tsx
+++ src/components/todos/Button.tsx
@@ -4,5 +4,12 @@ interface ButtonProps {
 }
 
 export const Button = ({ onClick, title }: ButtonProps) => {
-  return <button onClick={onClick}>{title}</button>;
+  return (
+    <button
+      onClick={onClick}
+      className="border-[1.5px] px-2 border-black rounded-md"
+    >
+      {title}
+    </button>
+  );
 };

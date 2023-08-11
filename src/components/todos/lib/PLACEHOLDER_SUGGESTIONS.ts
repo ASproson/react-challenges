41ff3f2b05946c0a91fc94c75393d8dd7c39129c
atlas <a.sproson@outlook.com>
2023-08-11 18:40:21 +0100

    Gets ASCII meme correct...

--- src/components/todos/lib/PLACEHOLDER_SUGGESTIONS.ts
+++ src/components/todos/lib/PLACEHOLDER_SUGGESTIONS.ts
@@ -7,7 +7,7 @@ export const PLACEHOLDER_SUGGESTIONS = [
   'Revive Angular',
   'Merge all the conflicts',
   'Master Git-fu',
-  'Think in binary, like in ASCII',
+  'Think in binary, live in ASCII',
   'Make Function calls, not phone calls',
   'Await the asyncs',
   'Abstract the abstractions',

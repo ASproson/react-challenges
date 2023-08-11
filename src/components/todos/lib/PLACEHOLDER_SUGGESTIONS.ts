738b117644efdfe0cfa3992200ce6c495ff0c231
atlas <a.sproson@outlook.com>
2023-08-11 15:51:05 +0100

    Abstracts data into lib

---
+++ src/components/todos/lib/PLACEHOLDER_SUGGESTIONS.ts
@@ -0,0 +1,15 @@
+export const PLACEHOLDER_SUGGESTIONS = [
+  'Study React',
+  'Fight with MegaLinter',
+  'Pick up {children}',
+  'Complete PR review',
+  'Explore the FKIT framework',
+  'Revive Angular',
+  'Merge all the conflicts',
+  'Master Git-fu',
+  'Think in binary, like in ASCII',
+  'Make Function calls, not phone calls',
+  'Await the asyncs',
+  'Abstract the abstractions',
+  'Determine if bug or feature',
+];

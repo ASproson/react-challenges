956cdbcf014e5df930885e7e865cf98e81a367f2
atlas <a.sproson@outlook.com>
2023-08-11 18:42:34 +0100

    Adds hover scaling to buttons

--- src/components/todos/ToDoActions.tsx
+++ src/components/todos/ToDoActions.tsx
@@ -19,19 +19,19 @@ export const ToDoActions = ({
     <div className="flex justify-center space-x-2 pt-4">
       <Button
         onClick={() => addToDo(newToDo)}
-        customStyling="bg-blue-400 px-2 p-1 rounded-md text-gray-200"
+        customStyling="bg-blue-400 px-2 p-1 rounded-md text-gray-200 hover:scale-105 transition ease-in-out"
       >
         Add ToDo
       </Button>
       <Button
-        customStyling="rounded-md bg-red-400 px-2 p-1 text-gray-200"
+        customStyling="rounded-md bg-red-400 px-2 p-1 text-gray-200 hover:scale-105 transition ease-in-out"
         onClick={() => handleRemoveLastToDo()}
       >
         Remove Last ToDo
       </Button>
 
       <Button
-        customStyling="rounded-md bg-emerald-400 px-2 p-1 text-gray-200"
+        customStyling="rounded-md bg-emerald-400 px-2 p-1 text-gray-200 hover:scale-105 transition ease-in-out"
         onClick={() => setShowCompletedToDos(!showCompletedToDos)}
       >
         Show Completed ToDos

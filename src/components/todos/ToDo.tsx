69156bfb819abce0f02691af20b322e0505c6791
atlas <a.sproson@outlook.com>
2023-08-11 17:43:30 +0100

    Uses fontawesome icons

--- src/components/todos/ToDo.tsx
+++ src/components/todos/ToDo.tsx
@@ -1,5 +1,7 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Button } from './Button';
 import { ToDoItem } from './Todos';
+import { faX, faCheck } from '@fortawesome/free-solid-svg-icons';
 
 interface ToDoProps {
   toDo: ToDoItem;
@@ -7,12 +9,18 @@ interface ToDoProps {
   completeToDo: (id: string) => void;
 }
 
+<FontAwesomeIcon icon={faX} />;
+
 export const ToDo = ({ toDo, deleteToDo, completeToDo }: ToDoProps) => {
   return (
     <div className="flex">
       <div>{toDo.title}</div>
-      <Button title="❌" onClick={() => deleteToDo(toDo.id)} />
-      <Button title="✔" onClick={() => completeToDo(toDo.id)} />
+      <Button onClick={() => deleteToDo(toDo.id)}>
+        <FontAwesomeIcon icon={faX} className="text-red-500" />
+      </Button>
+      <Button onClick={() => completeToDo(toDo.id)}>
+        <FontAwesomeIcon icon={faCheck} className="text-emerald-500" />
+      </Button>
     </div>
   );
 };

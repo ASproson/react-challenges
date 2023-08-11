14a92dbe7a28c95583ddeb3ffd379416e292b964
atlas <a.sproson@outlook.com>
2023-08-11 17:55:21 +0100

    Creates FontAwesomeIconWrapper

---
+++ src/components/todos/FontAwesomeIconWrapper.tsx
@@ -0,0 +1,23 @@
+import { IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import clsx from 'clsx';
+
+interface FontAwesomeIconWrapperProps {
+  icon: IconDefinition;
+  color?: string;
+}
+
+export const FontAwesomeIconWrapper = ({
+  icon,
+  color,
+}: FontAwesomeIconWrapperProps) => {
+  return (
+    <FontAwesomeIcon
+      icon={icon}
+      className={clsx(
+        'hover:scale-125 transition ease-in-out',
+        color && `text-${color}-500`
+      )}
+    />
+  );
+};

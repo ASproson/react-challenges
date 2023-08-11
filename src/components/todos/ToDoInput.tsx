f5f18ec4e6005341bcb510c427fabe2e684d96f5
atlas <a.sproson@outlook.com>
2023-08-11 15:31:59 +0100

    Introduces random placeholders

--- src/components/todos/ToDoInput.tsx
+++ src/components/todos/ToDoInput.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Button } from './Button';
 
 interface ToDoInputProps {
@@ -6,13 +7,36 @@ interface ToDoInputProps {
   addToDo: (e: string) => void;
 }
 
+const PLACEHOLDER_SUGGESTIONS = [
+  'Pick up dry cleaning',
+  'Study React',
+  'Fight with MegaLinter',
+  'Submit report',
+  'Pick up {...children}',
+];
+
+const randomSuggestion = () => {
+  return PLACEHOLDER_SUGGESTIONS[
+    Math.floor(Math.random() * PLACEHOLDER_SUGGESTIONS.length)
+  ];
+};
+
 export const ToDoInput = ({ newToDo, setNewToDo, addToDo }: ToDoInputProps) => {
+  const [placeholder, setPlaceholder] = useState(randomSuggestion());
+  const [currentToDo, setCurrentTodo] = useState(newToDo);
+
+  if (currentToDo !== newToDo) {
+    setPlaceholder(randomSuggestion());
+    setCurrentTodo(newToDo);
+  }
+
   return (
     <div className="flex">
       <input
         type="text"
         value={newToDo}
-        className="text-black"
+        className="text-black border-[1.5px] border-black rounded-md"
+        placeholder={placeholder}
         onChange={(e) => setNewToDo(e.target.value)}
         onKeyDown={(e) => e.key === 'Enter' && addToDo(newToDo)}
       />
@@ -21,3 +45,5 @@ export const ToDoInput = ({ newToDo, setNewToDo, addToDo }: ToDoInputProps) => {
     </div>
   );
 };
+
+// Animate placeholder text with array of suggestions?

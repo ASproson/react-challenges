1edb62aacf9124a8b043a1c6f1ad348fd017a65e
atlas <a.sproson@outlook.com>
2023-08-18 19:08:17 +0100

    Abstracts text input to own component

---
+++ src/components/textAnalyzer/TextInput.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import { Button } from '../todos/Button';
+
+export const TextInput = () => {
+  const [textInput, setTextInput] = useState('');
+  const [textLength, setTextLength] = useState(0);
+  const [textWordsCount, setTextWordsCount] = useState(0);
+  const [textCharactersCount, setTextCharactersCount] = useState(0);
+
+  const handleTextInput = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setTextInput(e.target.value);
+    setTextLength(textInput.length + 1);
+    setTextWordsCount(textInput.split(' ').length);
+    setTextCharactersCount(textInput.split(' ').join('').length + 1);
+  };
+
+  const handleTextAreaReset = () => {
+    setTextInput('');
+    setTextLength(0);
+    setTextWordsCount(0);
+    setTextCharactersCount(0);
+  };
+
+  return (
+    <div className="App">
+      <p>Text length: {textLength}</p>
+      <p>Word count: {textWordsCount}</p>
+      <p>Character count: {textCharactersCount}</p>
+
+      <textarea
+        onChange={(e) => handleTextInput(e)}
+        value={textInput}
+        className="border-2 border-black"
+      />
+
+      <Button onClick={handleTextAreaReset}>Clear Text Area</Button>
+    </div>
+  );
+};

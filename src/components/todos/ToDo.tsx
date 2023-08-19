11c4dd2c067d92cd48d1f6008f4d31a8549f791a
atlas <a.sproson@outlook.com>
2023-08-19 09:39:48 +0100

    Calls abstracted action buttons

--- src/components/todos/ToDo.tsx
+++ src/components/todos/ToDo.tsx
@@ -1,10 +1,8 @@
 import { useState } from 'react';
-import { Button } from './Button';
 import { ToDoItem } from './ToDosRoot';
 import { EditToDoInput } from './EditToDoInput';
 import { NotEditingToDo } from './NotEditingToDo';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faXmark, faCheck } from '@fortawesome/free-solid-svg-icons';
+import { ToDoActionButtons } from './ToDoActionButtons';
 
 interface ToDoProps {
   toDo: ToDoItem;
@@ -54,32 +52,6 @@ export const ToDo = ({
         deleteToDo={deleteToDo}
         completeToDo={completeToDo}
       />
-    </div>
-  );
-};
-
-interface ToDoActionButtonsProps {
-  id: string;
-  deleteToDo: (id: string) => void;
-  completeToDo: (id: string) => void;
-}
-
-export const ToDoActionButtons = ({
-  deleteToDo,
-  completeToDo,
-  id,
-}: ToDoActionButtonsProps) => {
-  return (
-    <div className="pl-2 space-x-2">
-      <Button defaultStyling={false} onClick={() => deleteToDo(id)}>
-        <FontAwesomeIcon icon={faXmark} className="text-red-500 font-bold" />
-      </Button>
-      <Button defaultStyling={false} onClick={() => completeToDo(id)}>
-        <FontAwesomeIcon
-          icon={faCheck}
-          className="text-emerald-500 font-bold"
-        />
-      </Button>
     </div>
   );
 };

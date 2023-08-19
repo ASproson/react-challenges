aea0cb7d6690a62d2dc30b1ed88e0a5edae0b1a2
atlas <a.sproson@outlook.com>
2023-08-19 09:01:16 +0100

    Adds 'card' styling

--- src/components/todos/ToDoList.tsx
+++ src/components/todos/ToDoList.tsx
@@ -15,18 +15,20 @@ export const ToDoList = ({
   completeToDo,
 }: ToDoListProps) => {
   return (
-    <div className="pt-4">
-      {toDos.map((toDo: ToDoItem) => {
-        return (
-          <ToDo
-            key={toDo.id}
-            toDo={toDo}
-            updateToDo={updateToDo}
-            deleteToDo={deleteToDo}
-            completeToDo={completeToDo}
-          />
-        );
-      })}
+    <div className="py-4 flex justify-center">
+      <div className="py-4 w-2/5 rounded-sm border-[1px] border-gray-400 shadow-md shadow-gray-400">
+        {toDos.map((toDo: ToDoItem) => {
+          return (
+            <ToDo
+              key={toDo.id}
+              toDo={toDo}
+              updateToDo={updateToDo}
+              deleteToDo={deleteToDo}
+              completeToDo={completeToDo}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };

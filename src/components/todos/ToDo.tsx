1a670bcb07721cff40d7e75063d2b6c2f61989ec
atlas <a.sproson@outlook.com>
2023-08-19 09:29:37 +0100

    Abstracts editing input into own component

--- src/components/todos/ToDo.tsx
+++ src/components/todos/ToDo.tsx
@@ -3,6 +3,7 @@ import { Button } from './Button';
 import { ToDoItem } from './ToDosRoot';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark, faCheck, faPencil } from '@fortawesome/free-solid-svg-icons';
+import { EditToDoInput } from './EditToDoInput';
 
 interface ToDoProps {
   toDo: ToDoItem;
@@ -62,32 +63,5 @@ export const ToDo = ({
         </Button>
       </div>
     </div>
-  );
-};
-
-interface EditToDoInputProps {
-  editedTitle: string;
-  onBlur: () => void;
-  isEditing: React.Dispatch<React.SetStateAction<boolean>>;
-  changeTitle: (event: React.ChangeEvent<HTMLInputElement>) => void;
-}
-
-export const EditToDoInput = ({
-  editedTitle,
-  onBlur,
-  isEditing,
-  changeTitle,
-}: EditToDoInputProps) => {
-  return (
-    <input
-      type="text"
-      value={editedTitle}
-      onChange={changeTitle}
-      onBlur={() => {
-        onBlur();
-        isEditing(false);
-      }}
-      className="text-center focus:border-gray-400 border-[1.5px] focus:outline-none focus:ring-0 rounded-md"
-    />
   );
 };

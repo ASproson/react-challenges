e190cb5f660b948338232d738639a6346b839414
atlas <a.sproson@outlook.com>
2023-09-09 09:36:39 +0100

    Adds react query wrapping

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,24 @@ import { NavBar } from './components/NavBar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { TextAnalyzerRoot } from './components/textAnalyzer/TextAnalyzerRoot';
 import { PokeCardsRoot } from './components/pokeCards/PokeCardsRoot';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/todos" element={<ToDosRoot />} />
-          <Route path="/textanalyzer" element={<TextAnalyzerRoot />} />
-          <Route path="/pokecards" element={<PokeCardsRoot />} />
-        </Routes>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/todos" element={<ToDosRoot />} />
+            <Route path="/textanalyzer" element={<TextAnalyzerRoot />} />
+            <Route path="/pokecards" element={<PokeCardsRoot />} />
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
     </div>
   );
 }
